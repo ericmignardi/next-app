ee1fb1afb8c336aa155669fec85947bca76097b1
Eric Mignardi <mignardi.e@gmail.com>
2026-06-25 10:52:05 -0400

    fix(auth): set proxyUrl on ClerkProvider to prevent client-side network/CORS errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <ClerkProvider>
+        <ClerkProvider proxyUrl="/__clerk">
                     <Toaster />
           {children}
         </ClerkProvider>
